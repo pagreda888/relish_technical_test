ec3f777b672379e6a5c29f9c0d7ce17322c41ed1
pagreda888 <pablo.agreda@galileo.edu>
2025-06-29 20:59:42 -0600

    testing the fetch of specific values from photos, using async and await instead of .then

--- api_endpoint/src/app.js
+++ api_endpoint/src/app.js
@@ -1,36 +1,59 @@
 const express = require("express");
-const router = express.Router();
 const axios = require("axios");
 
 const app = express();
 app.use(express.json());
 
 // test route - first response type var response
-app.get('/', (request, response) => {
-     const status = {
-      "Status": "Running"
-   };
-   
-   response.send(status);
-});
+app.get("/", (request, response) => {
+  const status = {
+    Status: "Running",
+  };
 
+  response.send(status);
+});
 
 // test api endpoint - second response type json alternative
-app.get('/api/test', (request, response) => {
-    response.json({ message: 'Demo endpoint', timestamp: new Date().toISOString() });
+app.get("/api/test", (request, response) => {
+  response.json({
+    message: "Demo endpoint",
+    timestamp: new Date().toISOString(),
+  });
 });
 
-// external API fetch test with axios
-app.get('/externalapi/users', (request, response) => {
-    axios
-        .get('https://jsonplaceholder.typicode.com/users')
-        .then(apiResponse => {
-            response.json(apiResponse.data);
-        })
-        .catch(error => {
-            console.error('Error fetching data from external API:', error);
-            response.status(500).json({ error: 'Failed to fetch data from external API' });
-        });
+// external API users fetch test with axios
+app.get("/externalapi/users", (request, response) => {
+  const API_URL = process.env.API_URL;
+  axios
+    .get(`${API_URL}/users/`)
+    .then((apiResponse) => {
+      response.json(apiResponse.data);
+    })
+    .catch((error) => {
+      console.error("Error fetching data from external API:", error);
+      response
+        .status(500)
+        .json({ error: "Failed to fetch data from external API" });
+    });
+});
+
+// external API users fetch an specific response value, we nedd to retreive ids's
+// for a cleaner aproach we can use async and wait
+app.get("/externalapi/photos/:id", async (request, response) => {
+  try {
+    const API_URL = process.env.API_URL;
+    const photo_id = request.params.id;
+    const apiResponse = await axios.get(`${API_URL}/photos/${photo_id}`);
+    response.json({
+      id: apiResponse.data.id,
+      title: apiResponse.data.title,
+    });
+  } catch (error) {
+    console.error("Error fetching data from external API:", error);
+    response
+      .status(500)
+      .json({ error: "Failed to fetch data from external API" });
+  }
 });
 
 module.exports = app;
